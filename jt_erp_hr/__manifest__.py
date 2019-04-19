498edca4d05d89a7d1be576551e32800e59f7875
Anil Kesariya <anil.r.kesariya@gmail.com>
2019-04-19 17:43:15 +0530

    [ADD]Added some modules

---
+++ jt_erp_hr/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Jupical Technologies Pvt. Ltd.
+#    Copyright (C) 2019-TODAY Jupical Technologies(<http://www.jupical.com>).
+#    Author: Jupical Technologies Pvt. Ltd.(<http://www.jupical.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Hr Apps Addon',
+    'summary': 'Install all bussiness applications in one click',
+    'version': '12.0.0.1.0',
+    'author': 'Jupical Technologies Pvt. Ltd.',
+    'maintainer': 'Jupical Technologies Pvt. Ltd.',
+    'website': 'http://www.jupical.com',
+    'license': 'AGPL-3',
+    'depends': ['hr_attendance','hr','hr_contract','hr_expense','hr_holidays',
+                'hr_timesheet','hr_recruitment','hr_expense_check','hr_gamification',
+                'hr_maintenance','hr_org_chart','hr_payroll','hr_recruitment_survey',
+                'hr_timesheet_attendance'
+                ],
+    'data': [],
+    'application': False,
+}
