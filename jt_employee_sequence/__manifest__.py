86a8fa254c9d3c7d98c0d787e2327f4ddb4591ca
Anil Kesariya <anil.r.kesariya@gmail.com>
2019-04-27 15:00:25 +0530

    [ADD]Added module that add sequence for employee

---
+++ jt_employee_sequence/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Jupical Technologies Pvt. Ltd.
+#    Copyright (C) 2019-TODAY Jupical Technologies(<http://www.jupical.com>).
+#    Author: Jupical Technologies Pvt. Ltd.(<http://www.jupical.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Employee Sequence Id',
+    'summary': 'Generate employee sequence id',
+    'version': '12.0.0.1.0',
+    'author': 'Jupical Technologies Pvt. Ltd.',
+    'maintainer': 'Jupical Technologies Pvt. Ltd.',
+    'website': 'http://www.jupical.com',
+    'license': 'AGPL-3',
+    'depends': ['hr'],
+    'data': ['views/emp_view.xml','views/emp_sequence_view.xml'],
+    'application': False,
+}
